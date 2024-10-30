2d6a3bdbd2c0032eb486b82ceae1f7497b0a2f40
Yosef Zeegdon <yhz@orsameach.com>
2024-10-30 14:50:12 +0200

    models and added user

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    birth_date = Column(Date)
+
