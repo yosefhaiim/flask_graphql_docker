b44544a97f28d5bce167615f6e6dd3d5fd546fc6
Yosef Zeegdon <yhz@orsameach.com>
2024-10-30 15:01:05 +0200

    database.py created

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models import Base
+
+
+connection_url = 'postgresql://'
+engin = create_engine(connection_url, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engin))
+
+
+def init_db():
+    import models
+    Base.metadata.create_all(bind=engin)
+
